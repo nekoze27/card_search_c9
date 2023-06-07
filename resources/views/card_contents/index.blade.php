df8327ceb8f2f141933dea980753a840c60957d1
岩崎幹央 <teketeke.jimmy@gmail.com>
2023-06-07 17:56:47 +0900

    一覧のテーブルを整えた

--- resources/views/card_contents/index.blade.php
+++ resources/views/card_contents/index.blade.php
@@ -1,54 +1,59 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class="front-semibold text-xl text-gray-800 leading-tight">
-      カード一覧
-    </h2>
-  </x-slot>
+  <div class="ml-4 mt-4 mb-4">
+    <x-slot name="header">
+      <h2 class="front-semibold text-xl text-gray-800 leading-tight">
+        カード一覧
+      </h2>
+    </x-slot>
 
-  <div class="flex item-center">
-    <form action="{{ route('card_search') }}" method="GET">
-      @csrf
-      <input type="text" name="name" placeholder="カード名">
-      <input type="text" name="category" placeholder="カテゴリー">
-      <x-primary-button>
-        検索
-      </x-primary-button>
-    </form>
-    @if (Auth::check() && auth()->user()->role == 'admin') 
-      <a href="{{ route('create') }}" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        新規作成
-      </a>
-    @endif
+    <div class="flex item-center">
+      <form action="{{ route('card_search') }}" method="GET">
+        @csrf
+        <input type="text" name="name" placeholder="カード名">
+        <input type="text" name="category" placeholder="カテゴリー">
+        <x-primary-button>
+          検索
+        </x-primary-button>
+      </form>
+      @if (Auth::check() && auth()->user()->role == 'admin') 
+        <a href="{{ route('create') }}" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          新規作成
+        </a>
+      @endif
+    </div>
   </div>
 
-  <div class="mx-auto px-6">
-    <table class="table-auto border-spacing-1">
-      <thead>
-        <tr>
-          <th>カードNo</th>
-          <th>カード名</th>
-          <th>カテゴリー</th>
-          <th>HP</th>
-          <th>弱点</th>
-          <th>抵抗力</th>
-          <th>逃げるエネルギー</th>
-          <th>効果</th>
-        </tr>
-      </thead>
-      @foreach($card_contents as $card_content)
-      <tbody>
-        <tr>
-          <td>{{$card_content->card_no}}</td>
-          <td>{{$card_content->name}}</td>
-          <td>{{$card_content->category}}</td>
-          <td>{{$card_content->hp}}</td>
-          <td>{{$card_content->weakness}}</td>
-          <td>{{$card_content->resistance}}</td>
-          <td>{{$card_content->escape_energy}}</td>
-          <td>{{$card_content->effect}}</td>
-        </tr>
-      </tbody>
-      @endforeach
-    </table>
-  </div>
+  <div class="mx-auto">
+    <div class="ml-4">
+      <table class="min-w-full border border-gray-300">
+        <thead>
+          <tr class="bg-gray-200">
+            <th class="py-2 px-4 border-b">カードNo</th>
+            <th class="py-2 px-4 border-b">カード名</th>
+            <th class="py-2 px-4 border-b">カテゴリー</th>
+            <th class="py-2 px-4 border-b">HP</th>
+            <th class="py-2 px-4 border-b">弱点</th>
+            <th class="py-2 px-4 border-b">抵抗力</th>
+            <th class="py-2 px-4 border-b">逃げるエネルギー</th>
+            <th class="py-2 px-4 border-b">効果</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($card_contents as $card_content)
+          <tr>
+            <td class="py-2 px-4 border-b text-center">{{$card_content->card_no}}</td>
+            <td class="py-2 px-4 border-b text-center">{{$card_content->name}}</td>
+            <td class="py-2 px-4 border-b text-center">{{$card_content->category}}</td>
+            <td class="py-2 px-4 border-b text-center">{{$card_content->hp}}</td>
+            <td class="py-2 px-4 border-b text-center">{{$card_content->weakness}}</td>
+            <td class="py-2 px-4 border-b text-center">{{$card_content->resistance}}</td>
+            <td class="py-2 px-4 border-b text-center">{{$card_content->escape_energy}}</td>
+            <td class="py-2 px-4 border-b text-center">{{$card_content->effect}}</td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+</div>
+
 </x-app-layout>
--- resources/views/card_search/search.blade.php
+++ resources/views/card_search/search.blade.php
@@ -1,23 +1,25 @@
 <x-app-layout>
-  <x-slot name="header">
-    <h2 class="front-semibold text-xl text-gray-800 leading-tight">
-      カード検索
-    </h2>
-  </x-slot>
-    <div class="flex item-center">
-      <form action="{{ route('card_search') }}" method="GET">
-        @csrf
-        <input type="text" name="name" placeholder="カード名">
-        <input type="text" name="category" placeholder="カテゴリー">
-        <x-primary-button>
-          検索
-        </x-primary-button>
-      </form>
-      @if (Auth::check() && auth()->user()->role == 'admin') 
-        <a href="{{ route('create') }}" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          新規作成
-        </a>
-      @endif
-    </div>
+  <div class="ml-4 mt-4 mb-4">
+    <x-slot name="header">
+      <h2 class="front-semibold text-xl text-gray-800 leading-tight">
+        カード検索
+      </h2>
+    </x-slot>
+      <div class="flex item-center">
+        <form action="{{ route('card_search') }}" method="GET">
+          @csrf
+          <input type="text" name="name" placeholder="カード名">
+          <input type="text" name="category" placeholder="カテゴリー">
+          <x-primary-button>
+            検索
+          </x-primary-button>
+        </form>
+        @if (Auth::check() && auth()->user()->role == 'admin') 
+          <a href="{{ route('create') }}" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            新規作成
+          </a>
+        @endif
+      </div>
+  </div>
     
 </x-app-layout>
