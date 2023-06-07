de7214fecbf20e27b2f0fffa9ad4cf5dcfbcdfac
岩崎幹央 <teketeke.jimmy@gmail.com>
2023-06-07 17:07:44 +0900

    検索と一覧ページに管理者のみ新規作成ボタンが表示されるように実装

--- resources/views/card_contents/index.blade.php
+++ resources/views/card_contents/index.blade.php
@@ -5,13 +5,21 @@
     </h2>
   </x-slot>
 
-  <form action="{{ route('card_search') }}" method="GET">
-    @csrf
-    <input type="text" name="name" placeholder="カード名">
-    <input type="text" name="category" placeholder="カテゴリー">
-    <x-primary-button>
-      検索
-    </x-primary-button>
+  <div class="flex item-center">
+    <form action="{{ route('card_search') }}" method="GET">
+      @csrf
+      <input type="text" name="name" placeholder="カード名">
+      <input type="text" name="category" placeholder="カテゴリー">
+      <x-primary-button>
+        検索
+      </x-primary-button>
+    </form>
+    @if (Auth::check() && auth()->user()->role == 'admin') 
+      <a href="{{ route('create') }}" class="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        新規作成
+      </a>
+    @endif
+  </div>
 
   <div class="mx-auto px-6">
     <table class="table-auto border-spacing-1">
